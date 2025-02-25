89dee78eea6f06a0fa484626e22fe4c070579014
fabianorpaula <fabiano.rpaula@gmail.com>
2025-02-25 20:42:07 -0300

    Mover Personagem

--- Assets/Scripts/Personagem.cs
+++ Assets/Scripts/Personagem.cs
@@ -20,4 +20,21 @@ public class Personagem : MonoBehaviour
     }
 
 
+    private void Update()
+    {
+        Mover();
+    }
+
+    void Mover()
+    {
+        Vector3 destino = Input.mousePosition;
+        Vector3 posTela = Camera.main.ScreenToWorldPoint(destino);
+        Vector3 posCorrecao = new Vector3(posTela.x, -4.05f, 0);
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            posCorrecao,
+            0.05f);
+    }
+
+
 }
